c95e63c38c5dd4efdaea41a250cd6b95693522d2
Al Hove <hoveax16@juniata.edu>
2019-02-24 15:06:21 -0500

    add

---
+++ ConnectFourWebApp/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ConnectFourWebApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.playerTurn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Location = new System.Drawing.Point(100, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 365);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // playerTurn
+            // 
+            this.playerTurn.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.playerTurn.Enabled = false;
+            this.playerTurn.ForeColor = System.Drawing.Color.Black;
+            this.playerTurn.Location = new System.Drawing.Point(291, 12);
+            this.playerTurn.Name = "playerTurn";
+            this.playerTurn.Size = new System.Drawing.Size(204, 13);
+            this.playerTurn.TabIndex = 0;
+            this.playerTurn.Text = "Player 1 turn";
+            this.playerTurn.TextChanged += new System.EventHandler(this.playerTurn_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playerTurn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox playerTurn;
+    }
+}
+
---
+++ ConnectFourWebApp/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectFourWebApp
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        SolidBrush pen = new SolidBrush(Color.FromArgb(250, 255, 255));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            g.FillEllipse(pen, 40, 10, 40, 40);
+            g.FillEllipse(pen, 40, 60, 40, 40);
+            g.FillEllipse(pen, 40, 110, 40, 40);
+            g.FillEllipse(pen, 40, 160, 40, 40);
+            g.FillEllipse(pen, 40, 210, 40, 40);
+            g.FillEllipse(pen, 40, 260, 40, 40);
+            g.FillEllipse(pen, 40, 310, 40, 40);
+            g.FillEllipse(pen, 120, 10, 40, 40);
+            g.FillEllipse(pen, 120, 60, 40, 40);
+            g.FillEllipse(pen, 120, 110, 40, 40);
+            g.FillEllipse(pen, 120, 160, 40, 40);
+            g.FillEllipse(pen, 120, 210, 40, 40);
+            g.FillEllipse(pen, 120, 260, 40, 40);
+            g.FillEllipse(pen, 120, 310, 40, 40);
+            g.FillEllipse(pen, 200, 10, 40, 40);
+            g.FillEllipse(pen, 200, 60, 40, 40);
+            g.FillEllipse(pen, 200, 110, 40, 40);
+            g.FillEllipse(pen, 200, 160, 40, 40);
+            g.FillEllipse(pen, 200, 210, 40, 40);
+            g.FillEllipse(pen, 200, 260, 40, 40);
+            g.FillEllipse(pen, 200, 310, 40, 40);
+            g.FillEllipse(pen, 280, 10, 40, 40);
+            g.FillEllipse(pen, 280, 60, 40, 40);
+            g.FillEllipse(pen, 280, 110, 40, 40);
+            g.FillEllipse(pen, 280, 160, 40, 40);
+            g.FillEllipse(pen, 280, 210, 40, 40);
+            g.FillEllipse(pen, 280, 260, 40, 40);
+            g.FillEllipse(pen, 280, 310, 40, 40);
+            g.FillEllipse(pen, 360, 10, 40, 40);
+            g.FillEllipse(pen, 360, 60, 40, 40);
+            g.FillEllipse(pen, 360, 110, 40, 40);
+            g.FillEllipse(pen, 360, 160, 40, 40);
+            g.FillEllipse(pen, 360, 210, 40, 40);
+            g.FillEllipse(pen, 360, 260, 40, 40);
+            g.FillEllipse(pen, 360, 310, 40, 40);
+            g.FillEllipse(pen, 440, 10, 40, 40);
+            g.FillEllipse(pen, 440, 60, 40, 40);
+            g.FillEllipse(pen, 440, 110, 40, 40);
+            g.FillEllipse(pen, 440, 160, 40, 40);
+            g.FillEllipse(pen, 440, 210, 40, 40);
+            g.FillEllipse(pen, 440, 260, 40, 40);
+            g.FillEllipse(pen, 440, 310, 40, 40);
+            g.FillEllipse(pen, 520, 10, 40, 40);
+            g.FillEllipse(pen, 520, 60, 40, 40);
+            g.FillEllipse(pen, 520, 110, 40, 40);
+            g.FillEllipse(pen, 520, 160, 40, 40);
+            g.FillEllipse(pen, 520, 210, 40, 40);
+            g.FillEllipse(pen, 520, 260, 40, 40);
+            g.FillEllipse(pen, 520, 310, 40, 40);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playerTurn_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
